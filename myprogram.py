de89e3ae4bf213e22f52ef952ee94f9b05b0e707
akira-kitajima-p <progressive@live.jp>
2025-01-17 00:32:46 +0900

    4.3まで読んだ。np.arrayの便利記法が直感から遠い位置にあってびっくりすることが多い・。・

--- myprogram.py
+++ myprogram.py
@@ -54,13 +54,49 @@ def init_network():
     network["b3"] = np.array([0.1, 0.2])
     return network
 
+def mean_squared_error(y,t):
+    return 0.5 * np.sum((y-t)**2)
+
+def cross_entropy_error(y,t):
+    delta = le-7 # log(0)を防ぐため
+    reutrn -np.sum(t * np.log(y + delta))
+
+# f(x)の微分を求める
+def numerical_diff(f,x):
+    h = le-4    # Doubleの誤差の問題で、あまりに小さい値にすると丸められてしまう。
+    # 1. f(x+h)-f(x)で求まる傾きは(x+h)とx間の傾きになってしまう。(本当はxの地点での傾きを求めたく、厳密にはhを0に限りなく近づける必要があるが、そうできない為)
+    # 2. (x+h)と(x-h)間の傾きを求めることで、xでの傾きを少ない誤差で求められるらしい
+    return (f(x+h) - f(x-h)) / (2*h) 
+
+
 # network = init_network()
 # x = np.array([1.0, 0.5])
 # y = forward(network, x)
 # print(y)
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
-img = x_train[0]
-img = img.reshape(28, 28)
 
-img_show(img)
+train_size = x_train.shape[0]
+batch_size = 3
+batch_mask = np.random.choice(train_size, batch_size)
+
+print(batch_mask)
+
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
+
+print(x_batch[0].size)
+
+y = np.array([[0, 0.2, 0.2 ,0.6, 0, 0, 0, 0, 0, 0], [0.1, 0.2, 0.2 ,0.4, 0, 0, 0, 0, 0, 0]])
+print(y.ndim)
+t = np.array(np.array([0, 0.2, 0.2 ,0.6, 0, 0, 0, 0, 0, 0]))
+
+print(np.arange(10))
+# print(y[np.arange(10), y])
+
+y = np.array([[0, 0.2, 0.2 ,0.6, 0, 0, 0, 0, 0, 0], [0.1, 0.2, 0.2 ,0.4, 0, 0, 0, 0, 0, 0]])
+print(y[[0,1], 3])
+
+# batch_size = 1
+# print(y[np.arange(batch_size), 3])
+# print(y[np.arange(batch_size), t])
